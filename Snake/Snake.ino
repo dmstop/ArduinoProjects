e332009ea4e2deb3372da4e6b534b6d9e5a01b84
Dmitry <dmitriy.stopkevich@gmail.com>
2022-04-23 15:46:43 +0400

    add

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -32,7 +32,10 @@ unsigned long anim_update;
 
 bool animOn = true;
 
-enum Direction { UP = 1, DOWN = 2, RIGHT = 3, LEFT = 4 };
+enum Direction { UP = 1,
+                 DOWN = 2,
+                 RIGHT = 3,
+                 LEFT = 4 };
 
 int foodX;
 int foodY;
@@ -56,80 +59,60 @@ Direction currentDir;
 
 int score;
 
-int getRate()
-{
+int getRate() {
   return map(analogRead(speedControl), 0, 1023, 100, 1200);
 }
 
-void showSnake()
-{
+void showSnake() {
   lc.setLed(0, body[bodyLength - 1][pbx], body[bodyLength - 1][pby], false);
 
   lc.setLed(0, headX, headY, true);
-  for (int i = 0; i < bodyLength; i++)
-  {
+  for (int i = 0; i < bodyLength; i++) {
     lc.setLed(0, body[i][bx], body[i][by], true);
   }
 }
 
-void moveSnake()
-{
+void moveSnake() {
   prevHx = headX;
   prevHy = headY;
 
-  if (currentDir == RIGHT)
-  {
+  if (currentDir == RIGHT) {
     headX++;
-  }
-  else if (currentDir == LEFT)
-  {
+  } else if (currentDir == LEFT) {
     headX--;
-  }
-  else if (currentDir == UP)
-  {
+  } else if (currentDir == UP) {
     headY++;
-  }
-  else if (currentDir == DOWN)
-  {
+  } else if (currentDir == DOWN) {
     headY--;
   }
 
-  if (headX == 8)
-  {
+  if (headX == 8) {
     headX = 0;
   }
-  if (headY == 8)
-  {
+  if (headY == 8) {
     headY = 0;
   }
-  if (headX == -1)
-  {
+  if (headX == -1) {
     headX = 7;
   }
-  if (headY == -1)
-  {
+  if (headY == -1) {
     headY = 7;
   }
 
-  for (int i = 0; i < bodyLength; i++)
-  {
+  for (int i = 0; i < bodyLength; i++) {
     body[i][pbx] = body[i][bx];
     body[i][pby] = body[i][by];
 
-    if (i == 0)
-    {
+    if (i == 0) {
       body[i][bx] = prevHx;
       body[i][by] = prevHy;
-    }
-    else
-    {
+    } else {
       body[i][bx] = body[i - 1][pbx];
       body[i][by] = body[i - 1][pby];
     }
   }
 
-  if (headX == foodX && headY == foodY)
-  {
+  if (headX == foodX && headY == foodY) {
     body[bodyLength][bx] = body[bodyLength - 1][bx];
     body[bodyLength][by] = body[bodyLength - 1][by];
     body[bodyLength][pbx] = body[bodyLength - 1][pbx];
@@ -145,58 +128,38 @@ void moveSnake()
   }
 }
 
-void readMovement()
-{
+void readMovement() {
   int mX = map(analogRead(joyY), 0, 1023, 10, -10);
   int mY = map(analogRead(joyX), 0, 1023, 10, -10);
 
-  if (abs(mX) > abs(mY))
-  {
-    if (mX > 8 && currentDir != LEFT)
-    {
+  if (abs(mX) > abs(mY)) {
+    if (mX > 8 && currentDir != LEFT) {
       currentDir = RIGHT;
-    }
-    else if (mX < -8 && currentDir != RIGHT)
-    {
+    } else if (mX < -8 && currentDir != RIGHT) {
       currentDir = LEFT;
     }
-  }
-  else
-  {
-    if (mY > 8 && currentDir != DOWN)
-    {
+  } else {
+    if (mY > 8 && currentDir != DOWN) {
       currentDir = UP;
-    }
-    else if (mY < -8 && currentDir != UP)
-    {
+    } else if (mY < -8 && currentDir != UP) {
       currentDir = DOWN;
     }
   }
 }
 
-void readMovementIR()
-{
+void readMovementIR() {
   if (IrReceiver.decode()) {
     int cmd = IrReceiver.decodedIRData.command;
 
-    if (cmd == 8 && currentDir != RIGHT)
-    {
+    if (cmd == 8 && currentDir != RIGHT) {
       currentDir = LEFT;
-    }
-    else if (cmd == 90 && currentDir != LEFT)
-    {
+    } else if (cmd == 90 && currentDir != LEFT) {
       currentDir = RIGHT;
-    }
-    else if (cmd == 24 && currentDir != DOWN)
-    {
+    } else if (cmd == 24 && currentDir != DOWN) {
       currentDir = UP;
-    }
-    else if (cmd == 82 && currentDir != UP)
-    {
+    } else if (cmd == 82 && currentDir != UP) {
       currentDir = DOWN;
-    }
-    else if (cmd == 22)
-    {
+    } else if (cmd == 22) {
       restart();
     }
 
@@ -204,21 +167,17 @@ void readMovementIR()
   }
 }
 
-bool checkGameOver()
-{
+bool checkGameOver() {
   bool gameOver = false;
 
-  for (int i = 0; i < bodyLength; i++)
-  {
-    if (body[i][bx] == headX && body[i][by] == headY)
-    {
+  for (int i = 0; i < bodyLength; i++) {
+    if (body[i][bx] == headX && body[i][by] == headY) {
       gameOver = true;
       break;
     }
   }
 
-  if (gameOver)
-  {
+  if (gameOver) {
     lcd.setCursor(3, 0);
     lcd.print("Game over!");
     lcd.setCursor(4, 1);
@@ -232,46 +191,35 @@ bool checkGameOver()
     delay(100);
     tone(soundPin, notes[3], 100);
 
-    for (int i = 0; i < 8; i++)
-    {
-      for (int j = 0; j < 8; j++)
-      {
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
         lc.setLed(0, j, i, true);
         delay(15);
       }
     }
 
     return true;
-  }
-  else
-  {
+  } else {
     return false;
   }
 }
 
-void genFood()
-{
-  do
-  {
+void genFood() {
+  do {
     foodX = random(0, 8);
     foodY = random(0, 8);
-  }
-  while (verFood() == false);
+  } while (verFood() == false);
 
   lc.setLed(0, foodX, foodY, true);
 }
 
-bool verFood()
-{
-  if (headX == foodX && headY == foodY)
-  {
+bool verFood() {
+  if (headX == foodX && headY == foodY) {
     return false;
   }
 
-  for (int i = 0; i < bodyLength; i++)
-  {
-    if (body[i][bx] == foodX && body[i][by] == foodY)
-    {
+  for (int i = 0; i < bodyLength; i++) {
+    if (body[i][bx] == foodX && body[i][by] == foodY) {
       return false;
     }
   }
@@ -279,16 +227,14 @@ bool verFood()
   return true;
 }
 
-void showScore()
-{
+void showScore() {
   lcd.setCursor(4, 0);
   lcd.print("Score: ");
   lcd.print(score);
   lcd.print("   ");
 }
 
-void restart()
-{
+void restart() {
   lcd.clear();
   lc.clearDisplay(0);
 
@@ -303,8 +249,7 @@ void restart()
 
   score = 0;
 
-  for (int i = 0; i < bodyLength; i++)
-  {
+  for (int i = 0; i < bodyLength; i++) {
     body[i][bx] = headX - i - 1;
     body[i][by] = headY;
 
@@ -323,20 +268,15 @@ void restart()
 }
 
 uint8_t btn_prev = LOW;
-bool readResBtn()
-{
+bool readResBtn() {
   uint8_t btn = digitalRead(resetBtn);
 
-  if (btn == HIGH)
-  {
-    if (btn_prev == LOW)
-    {
+  if (btn == HIGH) {
+    if (btn_prev == LOW) {
       btn_prev = HIGH;
       return true;
     }
-  }
-  else
-  {
+  } else {
     btn_prev = LOW;
   }
 
@@ -376,30 +316,24 @@ void setup() {
 void loop() {
   unsigned long currMil = millis();
 
-  if (currMil > game_update)
-  {
+  if (currMil > game_update) {
     moveSnake();
     showSnake();
 
     oldDir = currentDir;
     game_update += getRate();
 
-    if (checkGameOver())
-    {
+    if (checkGameOver()) {
       delay(5000);
       restart();
     }
   }
 
-  if (currMil > anim_update)
-  {
-    if (animOn == true)
-    {
+  if (currMil > anim_update) {
+    if (animOn == true) {
       lc.setLed(0, headX, headY, true);
       lc.setLed(0, foodX, foodY, false);
-    }
-    else
-    {
+    } else {
       lc.setLed(0, headX, headY, false);
       lc.setLed(0, foodX, foodY, true);
     }
@@ -408,13 +342,11 @@ void loop() {
     anim_update += ANIMRATE;
   }
 
-  if (readResBtn() == true)
-  {
+  if (readResBtn() == true) {
     restart();
   }
 
-  if (oldDir == currentDir)
-  {
+  if (oldDir == currentDir) {
     readMovement();
     readMovementIR();
   }
